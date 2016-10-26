6d0780b43e207cd0b8dcba8d94a02d813daee528
KT <truongmkevin@yahoo.com>
2016-10-26 15:52:55 +0000

    created song scaffold and created associations with artist

--- musicDB/db/schema.rb
+++ musicDB/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161025142332) do
+ActiveRecord::Schema.define(version: 20161026152148) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -20,5 +20,14 @@ ActiveRecord::Schema.define(version: 20161025142332) do
     t.datetime "updated_at",   null: false
     t.string   "instrument"
   end
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "songs", ["artist_id"], name: "index_songs_on_artist_id"
 
 end
